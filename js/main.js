004852e3f7562a3fcec1038ec5f151b40b0e4f9d
Amanda Ghassaei <amandaghassaei@gmail.com>
2014-09-14 06:23:14 -0400

    adding threjs

--- js/main.js
+++ js/main.js
@@ -6,12 +6,41 @@ $(document).ready(function(){
     tileSpace.tileWidth = 36;
 
     //init stuff - this should only run once
-    tileSpace.mainCanvas = Raphael(document.getElementById("svgContainer"), 900, 500);//main canvas
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-    tileSpace.tiles = [];
-    for (var i=0;i<12;i++){
-        for (var j=0;j<12;j++){
-            tileSpace.tiles.push(new Tile(i*(tileSpace.tileWidth+5), j*(tileSpace.tileWidth+5), [true, false], [false, true]))
-        }
-    }
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    var geometry = new THREE.BoxGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    var render = function () {
+        requestAnimationFrame(render);
+
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
+
+        renderer.render(scene, camera);
+    };
+
+    render();
+
+
+
+
+
+//    tileSpace.mainCanvas = Raphael(document.getElementById("svgContainer"), 900, 500);//main canvas
+//
+//    tileSpace.tiles = [];
+//    for (var i=0;i<12;i++){
+//        for (var j=0;j<12;j++){
+//            tileSpace.tiles.push(new Tile(i*(tileSpace.tileWidth+5), j*(tileSpace.tileWidth+5), [true, false], [false, true]))
+//        }
+//    }
 });
